31d1b06318be1fc3c83045ff64eeac9173a4fe79
RobertTheDev <robertthedev@gmail.com>
2023-07-06 22:26:19 +0100

    Set Up Page Layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent, LayoutComponent],
   imports: [CommonModule],
+  exports: [LayoutComponent],
 })
 export class LayoutModule {}
