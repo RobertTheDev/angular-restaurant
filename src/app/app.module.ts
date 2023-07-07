1ac35382a3086c2e179306ee24321efecf0f5aed
RobertTheDev <robertthedev@gmail.com>
2023-07-07 13:12:18 +0100

    Updated App Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,27 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { MenuItemsListComponent } from './menu-items-list/menu-items-list.component';
 import { HomeComponent } from './home/home.component';
-import { RestaurantPageComponent } from './restaurant-page/restaurant-page.component';
 import { GiftCardsComponent } from './gift-cards/gift-cards.component';
 import { MenusComponent } from './menus/menus.component';
 import { DetailsComponent } from './details/details.component';
 import { AboutComponent } from './about/about.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { EventModule } from './event/event.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuItemsListComponent,
     HomeComponent,
-    RestaurantPageComponent,
     GiftCardsComponent,
     MenusComponent,
     DetailsComponent,
     AboutComponent,
+    RestaurantComponent,
+    NotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, EventModule],
   providers: [],
   bootstrap: [AppComponent],
 })
