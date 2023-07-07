ef07dc94f6f915532cc0f2b4fd553fed17ca461d
RobertTheDev <robertthedev@gmail.com>
2023-07-07 12:28:34 +0100

    Created App Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RestaurantPageComponent } from './restaurant-page/restaurant-page.component';
+import { MenusComponent } from './menus/menus.component';
+import { DetailsComponent } from './details/details.component';
+import { GiftCardsComponent } from './gift-cards/gift-cards.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+  },
+  {
+    path: 'gift-cards',
+    component: GiftCardsComponent,
+  },
+  {
+    path: 'menus',
+    component: MenusComponent,
   },
   {
     path: 'restaurant',
