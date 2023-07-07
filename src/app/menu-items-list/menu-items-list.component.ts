8ea1546f382bf4d13857462741e7787e78530785
RobertTheDev <robertthedev@gmail.com>
2023-07-07 03:19:12 +0100

    Created Menus List And Imported Contentful

---
+++ src/app/menu-items-list/menu-items-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../contentful.service';
+
+@Component({
+  selector: 'app-menu-items-list',
+  templateUrl: './menu-items-list.component.html',
+  styleUrls: ['./menu-items-list.component.scss'],
+})
+export class MenuItemsListComponent implements OnInit {
+  menuItems: any = [];
+
+  constructor(private contentfulService: ContentfulService) {}
+
+  ngOnInit() {
+    this.contentfulService
+      .getAllEntries()
+      .then((menuItems) => (this.menuItems = menuItems));
+  }
+}
