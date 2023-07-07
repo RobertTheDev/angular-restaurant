0fb2c4a148875f83b08f2021b80ffe305cdaddd8
RobertTheDev <robertthedev@gmail.com>
2023-07-07 12:13:36 +0100

    Created Gift Cards Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LayoutModule } from './layout/layout.module';
 import { MenuItemsListComponent } from './menu-items-list/menu-items-list.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantPageComponent } from './restaurant-page/restaurant-page.component';
+import { GiftCardsComponent } from './gift-cards/gift-cards.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { RestaurantPageComponent } from './restaurant-page/restaurant-page.compo
     MenuItemsListComponent,
     HomeComponent,
     RestaurantPageComponent,
+    GiftCardsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
