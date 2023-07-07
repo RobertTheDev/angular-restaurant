0d0d74439516f0f2a1fdc286edb58aae5e19013b
RobertTheDev <robertthedev@gmail.com>
2023-07-07 01:55:59 +0100

    Added Environments To Contentful

--- src/app/contentful.service.ts
+++ src/app/contentful.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { createClient } from 'contentful';
 import { from } from 'rxjs';
-import { environment } from 'src/app/environments/environement';
+import { environment } from './environments/environment';
 
 @Injectable({
   providedIn: 'root',
