8f4e9f4a8b619a6560a1747bd6b1dcfa49ff4791
RobertTheDev <robertthedev@gmail.com>
2023-07-07 01:01:38 +0100

    Created Layout Styles

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
 
+const headerLinks = [
+  {
+    href: '/',
+    name: 'The Bar',
+  },
+  {
+    href: '/',
+    name: 'Gallery',
+  },
+  {
+    href: '/',
+    name: 'Home',
+  },
+  {
+    href: '/',
+    name: 'About',
+  },
+  {
+    href: '/',
+    name: 'Need To Know',
+  },
+  {
+    href: '/',
+    name: 'Menus',
+  },
+  {
+    href: '/',
+    name: 'Ticketed Events',
+  },
+  {
+    href: '/',
+    name: 'Private Dining',
+  },
+  {
+    href: '/',
+    name: 'Gift Cards',
+  },
+];
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  headerLinks = headerLinks;
+}
