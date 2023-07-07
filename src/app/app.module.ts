5891c8f9d7235c05b0c34caa5ede85714a018d37
RobertTheDev <robertthedev@gmail.com>
2023-07-07 06:01:16 +0100

    Created And Set Up Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { MenuItemsListComponent } from './menu-items-list/menu-items-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuItemsListComponent],
+  declarations: [AppComponent, MenuItemsListComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
