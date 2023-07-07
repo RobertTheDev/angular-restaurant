d5204b0ed1182ac4423787116d55ca9ba8c9a31c
RobertTheDev <robertthedev@gmail.com>
2023-07-07 13:10:43 +0100

    Created Event Module

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventComponent } from './components/events/event.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EventComponent],
   imports: [CommonModule],
 })
 export class EventModule {}
