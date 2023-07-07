19e5c6c022e4852d512e2ef23a0ba5da76c06d25
RobertTheDev <robertthedev@gmail.com>
2023-07-07 03:21:13 +0100

    Added Menu Items List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { MenuItemsListComponent } from './menu-items-list/menu-items-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuItemsListComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
