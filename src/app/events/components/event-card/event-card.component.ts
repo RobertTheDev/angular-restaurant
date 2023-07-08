796a7fd26d69b0153fcae9252462bdeedbc3e47b
RobertTheDev <robertthedev@gmail.com>
2023-07-08 10:31:37 +0100

    Created Event Card Component With Input

--- src/app/events/components/event-card/event-card.component.ts
+++ src/app/events/components/event-card/event-card.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import EventCard from 'src/app/interfaces/events/EventCard';
 
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.scss'],
 })
-export class EventCardComponent {}
+export class EventCardComponent {
+  @Input() event: EventCard = {
+    slug: '',
+    title: '',
+    subtitle: '',
+    image: {
+      uri: '',
+      alt: '',
+    },
+  };
+}
