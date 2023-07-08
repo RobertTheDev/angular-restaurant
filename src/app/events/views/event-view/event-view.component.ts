89e2fb0fdd4a3bb7c70e8feb23b06e24fa31859c
RobertTheDev <robertthedev@gmail.com>
2023-07-08 12:41:12 +0100

    Filtered Event Data Using Activated Route

--- src/app/events/views/event-view/event-view.component.ts
+++ src/app/events/views/event-view/event-view.component.ts
@@ -1,15 +1,31 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import eventsData from 'src/app/lib/data/events';
+import Event from 'src/app/interfaces/events/Event';
 
 @Component({
   selector: 'app-event-view',
   templateUrl: './event-view.component.html',
   styleUrls: ['./event-view.component.scss'],
 })
-export class EventViewComponent {
-  constructor(private location: Location) {}
+export class EventViewComponent implements OnInit {
+  slug: string | null = null;
+  event: Event | undefined = undefined;
+
+  constructor(
+    private location: Location,
+    public activatedRoute: ActivatedRoute,
+  ) {}
 
   goBack() {
     this.location.back();
+  }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.slug = params.get('slug');
+      this.event = eventsData.find((event) => event.slug === this.slug);
+    });
   }
 }
