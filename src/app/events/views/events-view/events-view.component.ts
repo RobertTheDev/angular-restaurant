5edef664dfd47faf5de5ad64fb52ec4128f6e076
RobertTheDev <robertthedev@gmail.com>
2023-07-08 10:32:06 +0100

    Mapped Event Card In Events View

--- src/app/events/views/events-view/events-view.component.ts
+++ src/app/events/views/events-view/events-view.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import eventsData from 'src/app/lib/data/events';
 
 @Component({
   selector: 'app-events-view',
   templateUrl: './events-view.component.html',
   styleUrls: ['./events-view.component.scss'],
 })
-export class EventsViewComponent {}
+export class EventsViewComponent {
+  events = eventsData;
+}
