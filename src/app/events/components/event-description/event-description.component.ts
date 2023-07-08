2188fa60596119fd6a7253e9b6807ee7d2c96180
RobertTheDev <robertthedev@gmail.com>
2023-07-08 12:53:19 +0100

    Deleted Event Description Component

--- src/app/events/components/event-description/event-description.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-event-description',
-  templateUrl: './event-description.component.html',
-  styleUrls: ['./event-description.component.scss'],
-})
-export class EventDescriptionComponent {}
