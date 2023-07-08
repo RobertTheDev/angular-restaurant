defbf292f6a08f1af1c8ff24a9e6f3227e44e14f
RobertTheDev <robertthedev@gmail.com>
2023-07-08 10:30:24 +0100

    Created Events Data

---
+++ src/app/lib/data/events.ts
@@ -0,0 +1,22 @@
+const eventsData = [
+  {
+    image: {
+      uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+      alt: '',
+    },
+    slug: 'private-party-for-ten-people',
+    title: 'Private party for 10-12 people',
+    subtitle: 'Private party for 10-12 people',
+  },
+  {
+    image: {
+      uri: 'https://images.unsplash.com/photo-1599582383719-907ff9af1a50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80',
+      alt: '',
+    },
+    slug: 'friends-family-table-for-four',
+    title: "Chef's table for four",
+    subtitle: "Chef's Table",
+  },
+];
+
+export default eventsData;
