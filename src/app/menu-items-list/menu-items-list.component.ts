b39f7f432b7a6108aacba469a9e827d763e1d414
RobertTheDev <robertthedev@gmail.com>
2023-07-08 14:07:23 +0100

    Added Category Filter To List

--- src/app/menu-items-list/menu-items-list.component.ts
+++ src/app/menu-items-list/menu-items-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ContentfulService } from '../services/contentful/contentful.service';
+import { Component, Input, OnInit } from '@angular/core';
+import menuItems from '../lib/data/menuItems';
+import MenuItem from '../interfaces/MenuItem';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-menu-items-list',
@@ -7,13 +9,18 @@ import { ContentfulService } from '../services/contentful/contentful.service';
   styleUrls: ['./menu-items-list.component.scss'],
 })
 export class MenuItemsListComponent implements OnInit {
-  menuItems: any = [];
+  menuItems: MenuItem[] = [];
 
-  constructor(private contentfulService: ContentfulService) {}
+  @Input() category: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.contentfulService
-      .getAllEntries()
-      .then((menuItems) => (this.menuItems = menuItems));
+    this.route.queryParams.subscribe((params) => {
+      this.category = params['category'];
+      this.menuItems = menuItems.filter(
+        (menuItem) => menuItem.category === this.category,
+      );
+    });
   }
 }
