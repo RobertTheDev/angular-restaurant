d8778aaca60422a1e4f3f59499fb76e7604812b8
RobertTheDev <robertthedev@gmail.com>
2023-07-08 11:13:11 +0100

    Mapped Testimonials Data Via Testimonial Card

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import testimonialsData from 'src/app/lib/data/testimonials';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  testimonials = testimonialsData;
+}
