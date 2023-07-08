64fa61df341200291699cb331664247dca615c29
RobertTheDev <robertthedev@gmail.com>
2023-07-08 08:50:12 +0100

    Created Event View Component

---
+++ src/app/events/views/event-view/event-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-view',
+  templateUrl: './event-view.component.html',
+  styleUrls: ['./event-view.component.scss'],
+})
+export class EventViewComponent {}
