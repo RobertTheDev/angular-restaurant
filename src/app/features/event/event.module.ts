d14c9e4290ce404c0a951bdf0dad76ff48350570
RobertTheDev <robertthedev@gmail.com>
2023-07-08 08:51:43 +0100

    Deleted Event Module

--- src/app/features/event/event.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EventComponent } from './components/events/event.component';
-
-@NgModule({
-  declarations: [EventComponent],
-  imports: [CommonModule],
-})
-export class EventModule {}
