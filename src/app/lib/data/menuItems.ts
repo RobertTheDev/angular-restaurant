3818ab80541990f5fd752e94f7d9a73a8087f242
RobertTheDev <robertthedev@gmail.com>
2023-07-08 13:15:07 +0100

    Created Menu Items Data

---
+++ src/app/lib/data/menuItems.ts
@@ -0,0 +1,26 @@
+import MenuItem from 'src/app/interfaces/MenuItem';
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Roman Army Sourdough',
+    description: 'sunflower spread, whipped lardo',
+    number: 1,
+    category: 'Dinner',
+    price: {
+      currency: 'GBP',
+      amount: 12.99,
+    },
+  },
+  {
+    name: 'Ice Cream',
+    description: 'sunflower spread, whipped lardo',
+    number: 13,
+    category: 'Desert',
+    price: {
+      currency: 'GBP',
+      amount: 5.99,
+    },
+  },
+];
+
+export default menuItems;
