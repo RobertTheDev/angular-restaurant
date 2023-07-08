6f1c74ad306ccc3e70aae54f239bf0e6eba9487a
RobertTheDev <robertthedev@gmail.com>
2023-07-08 14:07:50 +0100

    Added Menu Items List And Added Filter Input

--- src/app/features/menus/menus.component.ts
+++ src/app/features/menus/menus.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import menuCategories from 'src/app/lib/data/menuCategories';
 
 @Component({
   selector: 'app-menus',
   templateUrl: './menus.component.html',
   styleUrls: ['./menus.component.scss'],
 })
-export class MenusComponent {}
+export class MenusComponent {
+  menuCategories = menuCategories;
+
+  category: string | null = null;
+
+  constructor(private router: Router) {}
+
+  navigateToCategory(category: string) {
+    this.router.navigate([], { queryParams: { category: category } });
+  }
+}
