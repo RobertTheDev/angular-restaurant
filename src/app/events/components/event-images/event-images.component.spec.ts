d3d0a3790de27c79a1097af6773cb3eda15ccb4e
RobertTheDev <robertthedev@gmail.com>
2023-07-08 08:50:42 +0100

    Created Event Images Component

---
+++ src/app/events/components/event-images/event-images.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventImagesComponent } from './event-images.component';
+
+describe('EventImagesComponent', () => {
+  let component: EventImagesComponent;
+  let fixture: ComponentFixture<EventImagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EventImagesComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EventImagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/events/components/event-images/event-images.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-images',
+  templateUrl: './event-images.component.html',
+  styleUrls: ['./event-images.component.scss'],
+})
+export class EventImagesComponent {}
