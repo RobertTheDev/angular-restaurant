a28a9d43b81704d0d320f899dc7ae4ec96931c4d
RobertTheDev <robertthedev@gmail.com>
2023-07-08 10:44:37 +0100

    Added Images In Data

--- src/app/lib/data/events.ts
+++ src/app/lib/data/events.ts
@@ -1,21 +1,61 @@
-const eventsData = [
+import EventCard from 'src/app/interfaces/events/Event';
+
+const eventsData: EventCard[] = [
   {
-    image: {
-      uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-      alt: '',
-    },
+    images: [
+      {
+        // eslint-disable-next-line sonarjs/no-duplicate-string
+        uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+        alt: '',
+      },
+    ],
+    description:
+      'The Vault at Francie has fast become one of the premier private rooms in Brooklyn.  We would love to host a holiday celebration for you in this beautiful space of exposed brick and custom furnishings.',
     slug: 'private-party-for-ten-people',
     title: 'Private party for 10-12 people',
     subtitle: 'Private party for 10-12 people',
   },
   {
-    image: {
-      uri: 'https://images.unsplash.com/photo-1599582383719-907ff9af1a50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80',
-      alt: '',
-    },
+    images: [
+      {
+        // eslint-disable-next-line sonarjs/no-duplicate-string
+        uri: 'https://images.unsplash.com/photo-1599582383719-907ff9af1a50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1599582383719-907ff9af1a50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1599582383719-907ff9af1a50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80',
+        alt: '',
+      },
+      {
+        uri: 'https://images.unsplash.com/photo-1599582383719-907ff9af1a50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80',
+        alt: '',
+      },
+    ],
     slug: 'friends-family-table-for-four',
     title: "Chef's table for four",
     subtitle: "Chef's Table",
+    description:
+      'The Vault at Francie has fast become one of the premier private rooms in Brooklyn.  We would love to host a holiday celebration for you in this beautiful space of exposed brick and custom furnishings.',
   },
 ];
 
