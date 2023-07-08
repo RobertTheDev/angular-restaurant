f2f5d8826be5e04d4181ff190e4642627b192033
RobertTheDev <robertthedev@gmail.com>
2023-07-08 12:52:58 +0100

    Deleted Event Images Component

--- src/app/events/components/event-images/event-images.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-event-images',
-  templateUrl: './event-images.component.html',
-  styleUrls: ['./event-images.component.scss'],
-})
-export class EventImagesComponent {}
