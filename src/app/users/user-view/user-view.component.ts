d1f46e270977f650b0cd97191752d4dddfa252a2
RobertTheDev <robertthedev@gmail.com>
2023-07-09 15:41:16 +0100

    Used User Service To Fetch User By Id

--- src/app/users/user-view/user-view.component.ts
+++ src/app/users/user-view/user-view.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import User from 'src/app/interfaces/User';
+import { catchError, of, switchMap } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-view',
   templateUrl: './user-view.component.html',
   styleUrls: ['./user-view.component.scss'],
 })
-export class UserViewComponent {}
+export class UserViewComponent implements OnInit {
+  user: User | null = null;
+
+  constructor(
+    private usersService: UsersService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const userId = params.get('id');
+          if (userId) {
+            return this.usersService.getUserById(userId);
+          } else {
+            return of(null);
+          }
+        }),
+        catchError(() => of(null)),
+      )
+      .subscribe((data) => {
+        this.user = data;
+      });
+  }
+}
