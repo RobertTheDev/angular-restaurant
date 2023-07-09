163f241c7e19abfe3e087ed789881b543529414c
RobertTheDev <robertthedev@gmail.com>
2023-07-09 18:30:47 +0100

    Created Validators And Form Builder

--- src/app/users/components/create-user-form/create-user-form.component.ts
+++ src/app/users/components/create-user-form/create-user-form.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user-form',
   templateUrl: './create-user-form.component.html',
   styleUrls: ['./create-user-form.component.scss'],
 })
-export class CreateUserFormComponent {}
+export class CreateUserFormComponent {
+  profileForm = this.fb.group({
+    email: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    alert(JSON.stringify(this.profileForm.value));
+  }
+}
