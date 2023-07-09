ac98a33d23fd3e2edf3ea6df63059c9e3b1092fc
RobertTheDev <robertthedev@gmail.com>
2023-07-09 04:44:56 +0100

    Mapped Data From Service

--- src/app/users/users-view/users-view.component.ts
+++ src/app/users/users-view/users-view.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { catchError, of } from 'rxjs';
+import User from 'src/app/interfaces/User';
 
 @Component({
   selector: 'app-users-view',
   templateUrl: './users-view.component.html',
   styleUrls: ['./users-view.component.scss'],
 })
-export class UsersViewComponent {}
+export class UsersViewComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.usersService
+      .getUsers()
+      .pipe(catchError(() => of([])))
+      .subscribe((data) => {
+        this.users = data;
+      });
+  }
+}
