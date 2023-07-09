2466048542e7c8ad0c41ab6fe1377401be38ef49
RobertTheDev <robertthedev@gmail.com>
2023-07-09 11:06:12 +0100

    Moved Environments Folder

---
+++ environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  spaceId: 'xqma4xtbkyba',
+  accessToken: 'RgvFQcrmRTG12K-3OlxZFnMXnKC_J2rk76XJgVXe7GI',
+  mongoDBUrl: 'mongodb://127.0.0.1:27017',
+};
