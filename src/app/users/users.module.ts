214259871f299303696b3db7f0620924ee4014ae
RobertTheDev <robertthedev@gmail.com>
2023-07-09 15:33:33 +0100

    Added Router Module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { UsersViewComponent } from './users-view/users-view.component';
 import { UserViewComponent } from './user-view/user-view.component';
 import { UsersService } from './users.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [UsersViewComponent, UserViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   providers: [UsersService],
 })
 export class UsersModule {}
